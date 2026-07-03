d7766e256c0c16320b8072e8e09fd22b81d415fa
asnaw568 <asnawiyahkursiya@gmail.com>
2026-07-03 16:04:23 +0700

    Fix plugin bootstrap

---
+++ absensi-guru-mi.php
@@ -0,0 +1,16 @@
+require_once AGM_PLUGIN_PATH . 'app/Core/Plugin.php';
+require_once AGM_PLUGIN_PATH . 'app/Database/Installer.php';
+
+register_activation_hook(
+    __FILE__,
+    ['AGM\Database\Installer', 'install']
+);
+/**
+ * Jalankan Plugin
+ */
+function agm_run_plugin()
+{
+    return \AGM\Core\Plugin::get_instance();
+}
+
+agm_run_plugin();
